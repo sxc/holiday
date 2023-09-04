77a859abb99109174a32d58e94bdc6bc62d36563
Jim Shen <3212762+sxc@users.noreply.github.com>
2023-09-04 10:47:38 +0800

    add static route and modd conf

--- main.go
+++ main.go
@@ -1,17 +1,27 @@
 package main
 
 import (
-  "net/http"
+	"log"
+	"net/http"
 
-  "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-  r := gin.Default()
-  r.GET("/ping", func(c *gin.Context) {
-    c.JSON(http.StatusOK, gin.H{
-      "message": "pong",
-    })
-  })
-  r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	router := gin.Default()
+	// router.GET("/ping", func(c *gin.Context) {
+	// 	c.JSON(http.StatusOK, gin.H{
+	// 		"message": "pong",
+	// 	})
+	// })
+
+	// Static Routes
+	router.Static("/statics", "./statics")
+
+	// Static Routes to string
+	router.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Hello, world!")
+	})
+
+	log.Fatal(router.Run(":3000")) // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
