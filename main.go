931de3e2bb3b15aafdecd5a5f988dedda61128d7
Jim Shen <3212762+sxc@users.noreply.github.com>
2023-09-05 10:26:35 +0800

    add data validation

--- main.go
+++ main.go
@@ -4,15 +4,24 @@ import (
 	"embed"
 	"log"
 	"net/http"
-	"strconv"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
 )
 
 type TimeoffRequest struct {
-	Date   time.Time `json:"date" form:"date" binding:"-" time_format:"2006-01-02"`
-	Amount float64   `json:"amount" form:"amount" binding:"-"`
+	Date   time.Time `json:"date" form:"date" binding:"required,future" time_format:"2006-01-02"`
+	Amount float64   `json:"amount" form:"amount" binding:"required,gt=0"`
+}
+
+var ValidatorFuture validator.Func = func(f1 validator.FieldLevel) bool {
+	date, ok := f1.Field().Interface().(time.Time)
+	if ok {
+		return date.After(time.Now())
+	}
+	return true
 }
 
 //go:embed public/*
@@ -21,38 +30,9 @@ var f embed.FS
 func main() {
 	router := gin.Default()
 
-	// router.StaticFile("/", "./public/index.html")
-
-	// router.Static("/public", "./public")
-
-	// router.StaticFS("/fs", http.FileSystem(http.FS(f)))
-
-	// http.Request object
-	// router.GET("/*rest", func(c *gin.Context) {
-	// 	url := c.Request.URL.String()
-	// 	headers := c.Request.Header
-	// 	cookies := c.Request.Cookies()
-
-	// 	c.IndentedJSON(http.StatusOK, gin.H{
-	// 		"url":     url,
-	// 		"headers": headers,
-	// 		"cookies": cookies,
-	// 	})
-	// })
-
-	// http://localhost:3000/query/?username=john&year=2010&month=1&month=2
-
-	router.GET("/query/*rest", func(c *gin.Context) {
-		username := c.Query("username")
-		year := c.DefaultQuery("year", strconv.Itoa(time.Now().Year()))
-		months := c.QueryArray("month")
-
-		c.JSON(http.StatusOK, gin.H{
-			"username": username,
-			"year":     year,
-			"months":   months,
-		})
-	})
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("future", ValidatorFuture)
+	}
 
 	router.GET("/employee", func(c *gin.Context) {
 		c.File("./public/employee.html")
